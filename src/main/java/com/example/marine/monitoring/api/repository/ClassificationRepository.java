916ae8207549cf4826b3b6da039b9d55506bcbb4
GuilhermeAABarros <coringagui007@gmail.com>
2025-09-17 01:27:22 -0300

    add bd

--- src/main/java/com/example/marine/monitoring/api/repository/ClassificationRepository.java
+++ src/main/java/com/example/marine/monitoring/api/repository/ClassificationRepository.java
@@ -2,6 +2,8 @@ package com.example.marine.monitoring.api.repository;
 
 import com.example.marine.monitoring.api.entity.ClassificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassificationRepository extends JpaRepository<ClassificationEntity, Integer> {
 }
--- src/main/java/com/example/marine/monitoring/api/repository/ResidueRepository.java
+++ src/main/java/com/example/marine/monitoring/api/repository/ResidueRepository.java
@@ -2,6 +2,8 @@ package com.example.marine.monitoring.api.repository;
 
 import com.example.marine.monitoring.api.entity.ResidueEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResidueRepository extends JpaRepository<ResidueEntity, Integer> {
 }
