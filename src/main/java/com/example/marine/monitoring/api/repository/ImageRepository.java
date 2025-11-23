235a34ef916cbb899751daffff1c8b382b458c10
GuilhermeAABarros <coringagui007@gmail.com>
2025-11-23 16:36:44 -0300

    revome firebase-key

--- src/main/java/com/example/marine/monitoring/api/repository/ImageRepository.java
+++ src/main/java/com/example/marine/monitoring/api/repository/ImageRepository.java
@@ -1,6 +1,5 @@
 package com.example.marine.monitoring.api.repository;
 
-import com.example.marine.monitoring.api.entity.ClassificationEntity;
 import com.example.marine.monitoring.api.entity.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/marine/monitoring/api/repository/LocationImageRepository.java
+++ src/main/java/com/example/marine/monitoring/api/repository/LocationImageRepository.java
@@ -1,13 +1,9 @@
 package com.example.marine.monitoring.api.repository;
 
-import com.example.marine.monitoring.api.entity.ClassificationEntity;
 import com.example.marine.monitoring.api.entity.LocationImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
