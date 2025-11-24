ac5a3102d000ab2e6a12ae23da42629f83747678
GuilhermeAABarros <coringagui007@gmail.com>
2025-11-23 22:15:54 -0300

    add firebase-key

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM eclipse-temurin:17-jdk
 
+# Configura timezone e dependências do sistema e Python
 RUN apt-get update && \
     apt-get install -y tzdata && \
     ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime && \
@@ -7,6 +8,7 @@ RUN apt-get update && \
     apt-get install -y python3 python3-pip libgl1 libglib2.0-0 libsm6 libxrender1 libxext6 && \
     apt-get clean
 
+# Instala pacotes Python
 RUN pip install --no-cache-dir --break-system-packages \
     opencv-python \
     matplotlib \
@@ -18,9 +20,11 @@ RUN pip install --no-cache-dir --break-system-packages \
 
 WORKDIR /app
 
+# Copia JAR e modelos
 COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
 COPY modelo/ modelo/
 
+# Porta da aplicação
 EXPOSE 8081
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/example/marine/monitoring/api/FirebaseConfig.java
+++ src/main/java/com/example/marine/monitoring/api/FirebaseConfig.java
@@ -4,25 +4,26 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${spring.firebase.credentials}")
+    private String firebaseCredentialsPath;
+
     @PostConstruct
     public void init() throws IOException {
-        InputStream serviceAccount = getClass().getResourceAsStream("/firebase-key.json");
-
-        if (serviceAccount == null) {
-            throw new IllegalStateException("Firebase service account file not found!");
-        }
+        InputStream serviceAccount = new FileInputStream(firebaseCredentialsPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setStorageBucket("marinestore-bd39a.firebasestorage.app")
+                .setStorageBucket("marinestore-bd39a.appspot.com")
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
